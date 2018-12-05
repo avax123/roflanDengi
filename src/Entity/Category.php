bef9f2e3ab69fa65fbcb94aed59f1ef6ee072eb0
avax123 <36860205+avax123@users.noreply.github.com>
2018-12-05 10:04:39 +0200

    Update Category.php

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -1,85 +1,12 @@
 <?php
-
-namespace App\Entity;
-
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-
-/**
- * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
- * @UniqueEntity("name")
- */
-class Category
-{
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Payment", mappedBy="category", orphanRemoval=true)
-     */
-    private $payments;
-
-    public function __construct()
-    {
-        $this->payments = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Payment[]
-     */
-    public function getPayments(): Collection
-    {
-        return $this->payments;
-    }
-
-    public function addPayment(Payment $payment): self
-    {
-        if (! $this->payments->contains($payment)) {
-            $this->payments[] = $payment;
-            $payment->setCategory($this);
-        }
-
-        return $this;
-    }
-
-    public function removePayment(Payment $payment): self
-    {
-        if ($this->payments->contains($payment)) {
-            $this->payments->removeElement($payment);
-            // set the owning side to null (unless already changed)
-            if ($payment->getCategory() === $this) {
-                $payment->setCategory(null);
-            }
-        }
-
-        return $this;
-    }
-}
+namespace App\Entity;use Doctrine\Common\Collections\ArrayCollection;use Doctrine\Common\Collections\Collection;use Doctrine\ORM\Mapping as ORM;use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;class Category
+{private $id;private $name;private $payments;public function __construct()
+{$this->payments=new ArrayCollection()}public function getId():?int
+{return $this->id}public function getName():?string
+{return $this->name}public function setName(string $name):self
+{$this->name=$name;return $this}public function getPayments():Collection
+{return $this->payments}public function addPayment(Payment $payment):self
+{if(!$this->payments->contains($payment)){$this->payments[]=$payment;$payment->setCategory($this)}
+return $this}public function removePayment(Payment $payment):self
+{if($this->payments->contains($payment)){$this->payments->removeElement($payment);if($payment->getCategory()===$this){$payment->setCategory(null)}}
+return $this}}
