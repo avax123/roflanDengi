ec8dcc1a71c5b01f835d50bb8de4559d632ae854
avax123 <36860205+avax123@users.noreply.github.com>
2018-12-05 10:02:12 +0200

    Update PaymentController.php

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -12,50 +12,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class PaymentController extends AbstractController
 {
     /**
-     * @Route("/payments/create", name="payments.create")
-     *
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function create(Request $request): Response
-    {
-        $form = $this->createForm(PaymentCreateType::class);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var \App\Entity\Payment $payment */
-            $payment = $form->getData();
-            $payment->setUser($this->getUser());
-            $payment->setCreatedAt(new \DateTime());
-
-            $em = $this->getDoctrine()->getManager();
-
-            $em->persist($payment);
-            $em->flush();
-
-            return $this->redirectToRoute('payments.create');
-        }
-
-        return $this->render('payment/create.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/payments", name="payments.index")
-     *
-     * @param \App\Repository\PaymentRepository $paymentRepository
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function index(PaymentRepository $paymentRepository): Response
-    {
-        $payments = $paymentRepository->getLatest();
-
-        return $this->render('payment/index.html.twig', [
-            'payments' => $payments,
-        ]);
-    }
+    THIS IS IT
+    */
 }
